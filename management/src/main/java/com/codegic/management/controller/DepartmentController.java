295773481be8920d0fd9c5fe711f0df628c66ee0
Abdullah <l201326@lhr.nu.edu.pk>
2025-08-09 21:03:28 +0500

    Add Department Controller For Mappings

--- management/src/main/java/com/codegic/management/controller/DepartmentController.java
+++ management/src/main/java/com/codegic/management/controller/DepartmentController.java
@@ -1,5 +1,44 @@
 package com.codegic.management.controller;
 
+import com.codegic.management.dto.DepartmentDTO;
+import com.codegic.management.service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/departments")
 public class DepartmentController {
-    
+
+    private final DepartmentService departmentService;
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> create(@RequestBody DepartmentDTO dto) {
+        return new ResponseEntity<>(departmentService.createDepartment(dto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DepartmentDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(departmentService.getDepartment(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<?>> getAll() {
+        return ResponseEntity.ok(departmentService.getAllDepartments());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DepartmentDTO> update(@PathVariable Long id, @RequestBody DepartmentDTO dto) {
+        return ResponseEntity.ok(departmentService.updateDepartment(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        departmentService.deleteDepartment(id);
+        return ResponseEntity.noContent().build();
+    }
 }
