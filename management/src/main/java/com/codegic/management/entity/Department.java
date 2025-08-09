bdf1b804f6208a9d595510a9ea9016ed9437bd9a
Abdullah <l201326@lhr.nu.edu.pk>
2025-08-09 20:49:08 +0500

    Generate Entities for Management

--- management/src/main/java/com/codegic/management/entity/Department.java
+++ management/src/main/java/com/codegic/management/entity/Department.java
@@ -1,5 +1,19 @@
 package com.codegic.management.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Department {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String code;
 }
--- management/src/main/java/com/codegic/management/entity/Employee.java
+++ management/src/main/java/com/codegic/management/entity/Employee.java
@@ -1,5 +1,31 @@
 package com.codegic.management.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Employee {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @Positive
+    private Double salary;
+
+    @PastOrPresent
+    private LocalDate joiningDate;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
 }
