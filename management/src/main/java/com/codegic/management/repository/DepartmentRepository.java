fb09f80eff785ae0b6f7ac92109c8cdfd75bda19
Abdullah <l201326@lhr.nu.edu.pk>
2025-08-09 20:57:05 +0500

    Create Repositories for Classes

--- management/src/main/java/com/codegic/management/repository/DepartmentRepository.java
+++ management/src/main/java/com/codegic/management/repository/DepartmentRepository.java
@@ -1,5 +1,6 @@
 package com.codegic.management.repository;
 
-public class DepartmentRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.codegic.management.entity.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {}
--- management/src/main/java/com/codegic/management/repository/EmployeeRepository.java
+++ management/src/main/java/com/codegic/management/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.codegic.management.repository;
 
-public class EmployeeRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.codegic.management.entity.Employee;
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByDepartmentId(Long departmentId);
 }
